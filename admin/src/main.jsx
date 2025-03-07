7b3173beced5a65cfe738aa19e6df4faff2fb19d
KripaGurung <krepagurung@gmail.com>
2025-03-07 16:04:24 +0545

    Create UI of Admin login page

---
+++ admin/src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import AdminContextProvider from './contex/AdminContex.jsx'
+import DoctorContextProvider from './contex/DoctorContex.jsx'
+import AppContextProvider from './contex/AppContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <AdminContextProvider>
+      <DoctorContextProvider>
+        <AppContextProvider>
+          <App />
+        </AppContextProvider>
+      </DoctorContextProvider>
+    </AdminContextProvider>
+  </BrowserRouter>,
+)
---
+++ admin/tailwind.config.js
@@ -0,0 +1,12 @@
+export const content = [
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx,vue}", // Adjust based on your framework
+];
+export const theme = {
+  extend: {
+    colors:{
+      'primary': '#5F6FFF',
+    }
+  },
+};
+export const plugins = [];
